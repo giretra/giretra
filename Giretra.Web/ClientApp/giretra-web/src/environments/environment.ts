e7b1c3358a8a87a9f36be9d8c58ad99a1dfe3979
haga-rak <haga.rakotoharivelo@gmail.com>
2026-02-12 01:08:44 +0100

    Update default auth URL

--- Giretra.Web/ClientApp/giretra-web/src/environments/environment.ts
+++ Giretra.Web/ClientApp/giretra-web/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiBaseUrl: 'http://127.0.0.1:5067',
   hubUrl: 'http://127.0.0.1:5067/hubs/game',
   keycloak: {
-    url: 'http://localhost:8080',
+    url: 'https://auth.giretra.com',
     realm: 'giretra',
     clientId: 'giretra-web',
   },
